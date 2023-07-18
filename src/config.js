3f88df035e344254c5e29af8e677fdadeb1fda7b
Leevesh Kamboj <leeveshk12@gmail.com>
2023-07-18 14:31:17 +0530

    fix server address

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  serverUrl: "https://googleformscloneserver-production.up.railway.app",
+  serverUrl: "https://google-form-clone-server-1a0000075f04.herokuapp.com",
   untitledFormName: "Untitled form",
   type_options: ["Short Answer", "Long Answer", "Dropdown", "MCQ"],
   quetionTypesWithOptions: [2, 3],
